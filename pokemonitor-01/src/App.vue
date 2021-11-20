d3c8e22671d879a2c9d5726ddb561bf61accc8b9
Your Name <you@example.com>
2021-11-20 08:55:25 +0100

    Created new PokeMonitor project with routing and renamed old

---
+++ pokemonitor-01/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>Welcome to PokeMonitor!</div>
+  <NavbarIndex />
+  <PokemonCardGrid />
+</template>
+
+<script>
+import NavbarIndex from './components/NavbarIndex'
+import PokemonCardGrid from './components/PokemonCardGrid'
+
+export default {
+  name: 'App',
+  components: {
+    NavbarIndex,
+    PokemonCardGrid
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ pokemonitor-01/src/components/PokemonDetails.vue
@@ -0,0 +1,23 @@
+<template>
+    <NavbarDetails />
+</template>
+
+<script>
+import { ref } from 'vue';
+import NavbarDetails from './NavbarDetails'
+
+export default {
+    setup() {
+        const pokemonDetails = ref([]);
+        
+        return {
+            NavbarDetails,
+            pokemonDetails
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
